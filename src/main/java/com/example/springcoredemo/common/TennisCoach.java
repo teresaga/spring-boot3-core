8fe7c29c3d1a59473110e93489a6a566002f00d6
Teresa <teresagalaviza@gmail.com>
2025-02-21 16:25:52 -0700

    Primary annotation added

--- src/main/java/com/example/springcoredemo/common/TennisCoach.java
+++ src/main/java/com/example/springcoredemo/common/TennisCoach.java
@@ -1,5 +1,6 @@
 package com.example.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -2,7 +2,6 @@ package com.example.springcoredemo.rest;
 
 import com.example.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ public class DemoController {
     public Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach coach) {
+    public DemoController(Coach coach) {
         this.myCoach = coach;
     }
 
