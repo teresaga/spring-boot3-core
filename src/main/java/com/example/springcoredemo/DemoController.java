24da61fc4da4c583b731ce2324fb70aed70d99e0
Teresa <teresagalaviza@gmail.com>
2025-02-20 20:08:18 -0700

    Dependency Injection sample

---
+++ src/main/java/com/example/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define private field for the dependency
+    public Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach coach) {
+        this.myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
